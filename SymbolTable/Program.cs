20e2a009baf12c3435bfa6303f84e3597ffd5d33
Oleksandr1024 <assad34489@gmail.com>
2016-11-03 19:57:29 +0200

    First commit

---
+++ SymbolTable/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SymbolTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                       
+                 
+
+            Console.WriteLine("Введите нижнюю границу диапазона");
+            string n1 = Console.ReadLine();
+            Console.WriteLine("Введите верхнюю границу диапазона");
+            string n2 = Console.ReadLine();
+
+            int i = int.Parse(n1);
+            int s = int.Parse(n2);
+            
+            
+            for (; i < s; i++)
+            {
+                
+                    Console.Write("["+i+"] -"+(char)(i) + " ");               
+                
+            }
+            
+                   
+            Console.ReadKey();
+        }
+    }
+}
